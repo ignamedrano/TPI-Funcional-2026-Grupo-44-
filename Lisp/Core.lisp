4ce44cbb43bdc01a56fc05c04af14a771d6a6056
Magno <islermagno@ca.frre.utn.edu.ar>
2026-06-10 19:49:14 -0300

    new file:   Lisp/Core.lisp

---
+++ Lisp/Core.lisp
@@ -0,0 +1,50 @@
+
+(defun transicion-valida (color-actual cambiar-a)
+
+    (cond
+        ((and (eq color-actual 'rojo)(eq cambiar-a 'verde)) t)
+        ((and (eq color-actual 'verde)(eq cambiar-a 'amarillo)) t)
+        ((and (eq color-actual 'amarillo)(eq cambiar-a 'rojo)) t)
+        (t nil)
+    )
+)
+
+(defun transicion (color-actual cambiar-a)
+	(cond
+		((and (eq color-actual 'en-rojo) (eq cambiar-a 'verde))
+		(list 'en-verde "cambiar-a-verde"))
+
+		((and (eq color-actual 'en-amarillo) (eq cambiar-a 'rojo))(list 'en-rojo "cambiar-a-rojo"))
+
+		((and (eq color-actual 'en-verde) (eq cambiar-a 'amarillo))(list 'en-amarillo "cambiar-a-amarillo"))
+		(t (list color-actual 'accion-por-defecto))
+    )
+)
+
+(defun timer-rojo(tiempo-unix)
+    let ((actual-tiempo (tiempo-unix)))
+    (cond
+        ((<= (- actual-tiempo tiempo-unix) 90) 'rojo)
+        (t  timer-amarillo(tiempo-unix))
+    )
+)
+
+(defun timer-amarillo(tiempo-unix)
+    let ((actual-tiempo (tiempo-unix)))
+    (cond
+        ((<= (- actual-tiempo tiempo-unix) 6) 'amarillo)
+        (t timer-verde(tiempo-unix))
+    )
+)
+
+(defun  timer-verde(tiempo-unix)
+    let ((actual-tiempo (tiempo-unix)))
+    (cond
+        ((<= (- actual-tiempo tiempo-unix) 120) 'verde)
+        (t timer-rojo(tiempo-unix))
+    )
+)
+
+(defun timer (tiempo-unix)
+    timer-rojo(tiempo-unix)
+)
