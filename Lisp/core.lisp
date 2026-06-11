af9ce48c0a98e4341fff840f35e9b9adf575d2e2
Medrano Ignacio Nahuel <nachomedrano6@gmail.com>
2026-06-11 11:38:28 -0300

    Actualiza Requerimiento 1

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -7,14 +7,14 @@
 
 (defun transicion (color-actual cambiar-a)
 	(cond
-		((and (eq color-actual 'en-rojo) (eq cambiar-a 'verde))
-		(list 'en-verde "cambiar-a-verde"))
+		((and (eq color-actual 'en-rojo) (eq cambiar-a 'amarillo))
+		(list 'en-rojo "cambiar-a-amarillo"))
 
-		((and (eq color-actual 'en-amarillo) (eq cambiar-a 'rojo))
-        (list 'en-rojo "cambiar-a-rojo"))
+		((and (eq color-actual 'en-amarillo) (eq cambiar-a 'verde))
+        (list 'en-amarillo "cambiar-a-verde"))
 
-		((and (eq color-actual 'en-verde) (eq cambiar-a 'amarillo))
-        (list 'en-amarillo "cambiar-a-amarillo"))
+		((and (eq color-actual 'en-verde) (eq cambiar-a 'rojo))
+        (list 'en-verde "cambiar-a-rojo"))
 
 		(t (list color-actual 'accion-por-defecto))))
 
