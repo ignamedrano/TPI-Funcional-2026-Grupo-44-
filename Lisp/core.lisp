f1cc99c646d6c0dd56cee87fcd48e3e4dd899d3d
Martinezjuanesok <99510846+Martinezjuanesok@users.noreply.github.com>
2026-06-12 18:41:38 -0300

    agregando requerimiento 4 modificado

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -1,1 +1,13 @@
+(defun duracion-ciclo(s-rojo s-verde s-amarillo)
+       (+ s-rojo s-verde s-amarillo))
 
+(defun recomendacion-ciclo (duracion)
+       (let ((duracion-ciclo duracion))
+       (cond
+         ((< duracion 35)
+     "RECOMENDACION: Aumentar duracion del ciclo")
+
+     ((<= duracion 150)
+     "RECOMENDACION: Mantener duracion")
+
+         (t "RECOMENDACION: Reducir duracion del ciclo"))))
