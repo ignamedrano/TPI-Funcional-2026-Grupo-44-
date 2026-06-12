3359cb921831ea211d0806e7c8e7ef64994bf336
angelinnamartemucci-cloud <angelinnamartemucci@gmail.com>
2026-06-12 20:18:54 -0300

    Update core.lisp

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -1,19 +1,24 @@
-(defun timer (tiempo-unix)
-  (let ((tiempo-ciclo (mod tiempo-unix 225)))
-    (cond
-      ((< tiempo-ciclo 90) 'rojo)
-      ((< tiempo-ciclo 93) 'rojo-intermitente)
-      ((< tiempo-ciclo 213) 'verde)
-      ((< tiempo-ciclo 216) 'verde-intermitente)
-      ((< tiempo-ciclo 222) 'amarillo)
-      (t 'amarillo-intermitente))))
+;; FUNCIÓN: log-transicion
+;; NATURALEZA: No pura
+;; ESTRATEGIA: Recursiva
+;; IMPACTO: No destructiva
+;; PROPÓSITO: Registrar e imprimir en pantalla cada cambio
+;;            de estado del semáforo indicando el tiempo,
+;;            el color anterior y el nuevo color. 
 
 (defun log-transicion (tiempo-actual tiempo-fin color-anterior)
   (let ((color-nuevo (timer tiempo-actual)))
-  (cond
-    ((> tiempo-actual tiempo-fin) 'no-hay-cambios)
-           ((not (eq color-anterior color-nuevo))
-            (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%"
-                    tiempo-actual color-anterior color-nuevo)
-            (log-transicion (+ tiempo-actual 1) tiempo-fin color-nuevo))
-           (t (log-transicion (+ tiempo-actual 1) tiempo-fin color-anterior)))))
+    (cond
+      ((> tiempo-actual tiempo-fin)
+       'no-hay-cambios)
+
+      ((not (eq color-anterior color-nuevo))
+       (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%"
+               tiempo-actual color-anterior color-nuevo)
+       (log-transicion (+ tiempo-actual 1)
+                       tiempo-fin
+                       color-nuevo))
+
+      (t (log-transicion (+ tiempo-actual 1)
+                       tiempo-fin
+                       color-anterior)))))
