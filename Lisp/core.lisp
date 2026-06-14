073758b63c6a84bc239c9ed8d472eb7547a9525f
vnahuel454 <vifana454@gmail.com>
2026-06-14 06:10:08 +0200

    Update core.lisp req3 extension1

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -7,14 +7,6 @@
 
 
 
-
-
-
-
-
-
-
-
 ;; =========================================================
 ;; FUNCIÓN: timer
 ;; NATURALEZA: Pura (Dado un mismo número de tiempo UNIX, siempre retorna el mismo símbolo de color)
@@ -47,7 +39,17 @@
 
 
 
+;req 3 extension1
 
+(defun log-transicion (tiempo-actual tiempo-fin color-anterior)
+  (let ((color-nuevo (timer tiempo-actual)))
+  (cond
+    ((> tiempo-actual tiempo-fin) 'no-hay-cambios)
+           ((not (eq color-anterior color-nuevo))
+            (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%"
+                    tiempo-actual color-anterior color-nuevo)
+            (log-transicion (+ tiempo-actual 1) tiempo-fin color-nuevo))
+           (t (log-transicion (+ tiempo-actual 1) tiempo-fin color-anterior)))))
 
 
 
