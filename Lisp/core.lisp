2b95083b254c48f49a36423133fec39a6b92b5b2
vnahuel454 <vifana454@gmail.com>
2026-06-14 04:04:18 +0200

    Update core.lisp-req2 extension1

--- Lisp/core.lisp
+++ Lisp/core.lisp
@@ -30,3 +30,31 @@
       (t 'amarillo))
   )
 )
+
+
+
+
+; req 2 extension1
+(defun timer (tiempo-unix)
+  (let ((tiempo-ciclo (mod tiempo-unix 225)))
+    (cond
+      ((< tiempo-ciclo 90) 'rojo)
+      ((< tiempo-ciclo 93) 'rojo-intermitente)
+      ((< tiempo-ciclo 213) 'verde)
+      ((< tiempo-ciclo 216) 'verde-intermitente)
+      ((< tiempo-ciclo 222) 'amarillo)
+      (t 'amarillo-intermitente))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
